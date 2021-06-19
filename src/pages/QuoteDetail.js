d109eeab0c4df5e118855bfb5c0425751b3f77ef
Corey <cwills788@gmail.com>
2021-06-19 10:51:10 -0500

    first commit

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,67 @@
+import { Fragment, useEffect } from 'react'
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom'
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import Comments from '../components/comments/Comments'
+import useHttp from '../hooks/use-http'
+import { getSingleQuote } from '../lib/api'
+import LoadingSpinner from '../components/UI/LoadingSpinner'
+
+// const DUMMY_QUOTES = [
+//   { id: 'q1', author: 'Corey', text: 'Learning is fun!' },
+//   { id: 'q2', author: 'Max', text: 'Learning react is great!' },
+// ]
+
+const QuoteDetail = () => {
+  const match = useRouteMatch()
+  const params = useParams()
+
+  const { quoteId } = params
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true)
+
+  useEffect(() => {
+    sendRequest(quoteId)
+  }, [sendRequest, quoteId])
+
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    )
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  )
+}
+
+export default QuoteDetail
+
+// whatever we choose as the dynamic ':identifier', thats the key we can use with params
